68a532da24b2cfdb59c78d53b6601800f69ec520
Abdulmelik SAK <melik.sak@icloud.com>
2020-10-19 16:32:58 +0200

    exo finit avec variable global pas avec cookies

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pry-byebug'
 require 'better_errors'
+require 'nokogiri'
+require 'open-uri'
+require 'sinatra/cookies'
 require_relative 'lib/cookbook'
 require_relative 'lib/recipe'
+require_relative 'lib/scrapservice'
 set :bind, '0.0.0.0'
 
 configure :development do
@@ -37,5 +41,24 @@ end
 get '/markdone/:i' do
   cookbook = Cookbook.new('lib/recipes.csv')
   cookbook.mark_as_read(params[:i].to_i)
+  redirect '/'
+end
+
+get '/import' do
+  erb :import
+end
+
+post '/scraped' do
+  scrap = ScrapService.new(params[:ingredient])
+  $scraped = scrap.call
+  @scraped = $scraped
+  # cookies[:scraped] = scrap.call
+  erb :scrap
+end
+
+get '/add_import/:index' do
+  cookbook = Cookbook.new('lib/recipes.csv')
+  recipe = Recipe.new($scraped[params[:index.to_s].to_i])
+  cookbook.add_recipe(recipe)
   redirect '/'
 end
